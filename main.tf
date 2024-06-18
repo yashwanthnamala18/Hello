05adbc58c275c01a9fe03b1de07e52769ac2b733
ML-Mounika <159158311+ML-Mounika@users.noreply.github.com>
2024-06-18 22:15:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,67 +1,8 @@
-variable "instance_name" {
-	description = "The EC2 instance name"
-	default = "postgress"
-}
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket9234"
 
-variable "postgres_port" {
-	description = "The port number used postgres database"
-	default = 5432
-}
-
-variable "postgres_password" {
-	description = "The password used for postgres database"
-	default = "welcome1"
-}
-
-data "aws_availability_zones" "all" {}
-
-provider "aws" {
-	region = "us-east-1"
-}
-
-resource "aws_instance" "postgress" {
-  ami = "ami-0c6b1d09930fac512"
-  instance_type = "t2.micro"
-  
-  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
-  key_name = "joel-ec2-kp"
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo yum update -y
-              sudo yum install -y docker
-			  sudo service docker start
-			  sudo usermod -aG docker ec2-user
-			  sudo docker run --name postgres-fd -p ${var.postgres_port}:5432 -e POSTGRES_PASSWORD=${var.postgres_password} -e POSTGRES_DB=flexdeploy -d joelwenzel/postgres-fd:latest
-              EOF
-  tags {
-    Name = "${var.instance_name}"
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
   }
-}
-
-resource "aws_security_group" "instance" {
-  name = "flexagon-instance"
-  ingress {
-    from_port = "${var.postgres_port}"
-    to_port = "${var.postgres_port}"
-    protocol = "tcp"
-    cidr_blocks = ["98.102.83.134/32"]
-  }
-  
-  ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["98.102.83.134/32"]
-  }
-  
-  egress {
-		from_port   = 0
-		to_port     = 0
-		protocol    = "-1"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
-}
-
-output "postgres_url" {
-  value = "jdbc:postgresql://${aws_instance.postgress.public_ip}:${var.postgres_port}/flexdeploy"
 }
