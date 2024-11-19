11715536b90902eebd46c1b518f1f587bc7f5d4f
ML-Mounika <159158311+ML-Mounika@users.noreply.github.com>
2024-11-19 18:45:02 +0530

    Create main.tf

---
+++ Terraform/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+region = "us-east-1"
+}
+resource "aws_instance" "instance1" {
+  ami           = "ami-08a0d1e16fc3f61ea"
+  instance_type = "t2.micro"
+
+  tags = {
+    name = "My-Demo-Instances-Demo"
+  }
+}
