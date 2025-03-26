b9821a2a7f50649f6c871c66ec4ae0c14b5149d6
ML-Mounika <159158311+ML-Mounika@users.noreply.github.com>
2025-03-26 13:43:51 +0530

    Create main.tf

---
+++ main/main.tf
@@ -0,0 +1,19 @@
+terraform {
+    
+	backend "s3" {
+ bucket = "my-tf-test-bucket9234"
+ key    = "MLMounikaTest6/awsDemo/terraform.tfstate"
+ region = "us-east-1"
+  }
+}
+provider "aws" {
+region = "us-east-1"
+}
+resource "aws_instance" "instance1" {
+  ami           = "ami-08a0d1e16fc3f61ea"
+  instance_type = "t2.micro"
+
+  tags = {
+    name = "My-Demo-Instances-Demo"
+  }
+}
