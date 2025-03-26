613082f50791bd5c2422217dec79e032f1cf16c1
ML-Mounika <159158311+ML-Mounika@users.noreply.github.com>
2025-03-26 17:05:42 +0530

    Update main.tf

--- Replace/main/main.tf
+++ Replace/main/main.tf
@@ -11,7 +11,7 @@ region = "us-east-1"
 }
 resource "aws_instance" "instance1" {
   ami           = "ami-08a0d1e16fc3f61ea"
-  instance_type="${{TERRAFORMTYPE}}"
+  instance_type = "${{TERRAFORMTYPE}}"
 
   tags = {
     name = "My-Demo-Instances-Demo"
