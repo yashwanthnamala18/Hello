744c8f1c3c9b683add47712d1bb6a818a40af54e
ML-Mounika <159158311+ML-Mounika@users.noreply.github.com>
2025-03-26 14:05:12 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -3,7 +3,7 @@ region = "us-east-1"
 }
 resource "aws_instance" "instance1" {
   ami           = "ami-08a0d1e16fc3f61ea"
-  instance_type = "${{Intancetype}}"
+  instance_type = "${{InstanceType}}"
 
   tags = {
     name = "My-Demo-Instances-Demo"
