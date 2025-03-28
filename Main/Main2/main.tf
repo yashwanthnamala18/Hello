35a6b9788e17cf10efba5e11984cfaf84914abe9
ML-Mounika <159158311+ML-Mounika@users.noreply.github.com>
2025-03-28 17:59:10 +0530

    Update main.tf

--- Main/Main2/main.tf
+++ Main/Main2/main.tf
@@ -3,7 +3,7 @@ region = "us-east-1"
 }
 resource "aws_instance" "instance1" {
   ami           = "ami-08a0d1e16fc3f61ea"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     name = "My-Demo-Instances-Demo"
